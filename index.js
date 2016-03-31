3d28cd534d6f5dd4f1693ffb1606f0a0ee95e900
Robert DeLuca <robertdeluca@nat-oitwireless-inside-vapornet100-e-1279.Princeton.EDU>
2016-03-31 14:41:50 -0400

    hi again

--- index.js
+++ index.js
@@ -22,4 +22,47 @@ function dummy(text, callback) {
   callback(text);
 }
 
+
+
+// taken from the amazone example page:
+// http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+
+var params = {
+    Bucket: 'STRING_VALUE', /* required */
+    Key: 'STRING_VALUE', /* required */
+    ACL: 'private | public-read | public-read-write | authenticated-read | aws-exec-read | bucket-owner-read | bucket-owner-full-control',
+    Body: new Buffer('...') || 'STRING_VALUE' || streamObject,
+    CacheControl: 'STRING_VALUE',
+    ContentDisposition: 'STRING_VALUE',
+    ContentEncoding: 'STRING_VALUE',
+    ContentLanguage: 'STRING_VALUE',
+    ContentLength: 0,
+    ContentMD5: 'STRING_VALUE',
+    ContentType: 'STRING_VALUE',
+    Expires: new Date || 'Wed Dec 31 1969 16:00:00 GMT-0800 (PST)' || 123456789,
+    GrantFullControl: 'STRING_VALUE',
+    GrantRead: 'STRING_VALUE',
+    GrantReadACP: 'STRING_VALUE',
+    GrantWriteACP: 'STRING_VALUE',
+    Metadata: {
+	someKey: 'STRING_VALUE',
+	/* anotherKey: ... */
+    },
+    RequestPayer: 'requester',
+    SSECustomerAlgorithm: 'STRING_VALUE',
+    SSECustomerKey: new Buffer('...') || 'STRING_VALUE',
+    SSECustomerKeyMD5: 'STRING_VALUE',
+    SSEKMSKeyId: 'STRING_VALUE',
+    ServerSideEncryption: 'AES256 | aws:kms',
+    StorageClass: 'STANDARD | REDUCED_REDUNDANCY | STANDARD_IA',
+    WebsiteRedirectLocation: 'STRING_VALUE'
+};
+s3.putObject(params, function(err, data) {
+	if (err) console.log(err, err.stack); // an error occurred
+	else     console.log(data);           // successful response
+    });
+
+
+
+
 exports.dummy = dummy;
