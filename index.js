2916fde5828ff8e76543807681452fba32bb7f57
Robert DeLuca <robertdeluca@nat-oitwireless-inside-vapornet100-e-1279.Princeton.EDU>
2016-04-07 11:57:14 -0400

    most recent

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 var self = require("sdk/self");
-
+var URL = require('sdk/url').URL;
 var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
-
+//var thing = 'document.body.innerHTML = "<h1>localhost:5000</h1>";'
 var button = buttons.ActionButton({
   id: "mozilla-link",
   label: "Visit Mozilla",
@@ -16,7 +16,10 @@ var button = buttons.ActionButton({
 });
 
 function handleClick(state) {
-  tabs.open("localhost:5000");
+  //http://stackoverflow.com/questions/11594576/getting-current-browser-url-in-firefox-addon
+var idea = URL(tabs.activeTab.url);
+  // console.log(idea);
+tabs.open( "localhost:5000/" + idea);
 }
 //ZOMGsocool
 function dummy(text, callback) {
